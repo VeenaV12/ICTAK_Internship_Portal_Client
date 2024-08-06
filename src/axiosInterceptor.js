b134df18001dfc6c5f14616f05af370f53339489
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-08-06 11:46:59 +0530

    base url change

--- src/axiosInterceptor.js
+++ src/axiosInterceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ictak-internship-portal-server-alpha.vercel.app',
+  baseURL: 'https://ictak-internship-portal-server-alpha.vercel.app/api',
 });
 
 axiosInstance.interceptors.request.use(
--- src/components/project dashboard/axiosintercepter.js
+++ src/components/project dashboard/axiosintercepter.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://ictak-internship-portal-server-alpha.vercel.app/api'
 });
 
 axiosInstance.interceptors.request.use(
